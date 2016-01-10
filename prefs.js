2338aed56f5d4cb91073e053e89f469430e7d57a
Vaillant <vaillant.jeremy@dev-crea.com>
2016-01-10 17:54:17 +0100

    Add settings option

---
+++ prefs.js
@@ -0,0 +1,28 @@
+const GObject = imports.gi.GObject;
+const Gtk = imports.gi.Gtk;
+const Gio = imports.gi.Gio;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const Utils = Me.imports.utils;
+
+const Gettext = imports.gettext.domain('space');
+const _ = Gettext.gettext;
+
+let settings;
+
+function init() {
+    settings = Utils.getSettings(Me);
+}
+
+function buildPrefsWidget() {
+    // Prepare labels and controls settings windows
+    let buildable = new Gtk.Builder();
+    buildable.add_from_file(Me.dir.get_path() + '/prefs.xml');
+    let box = buildable.get_object('vbox_build');
+
+    // Bind fields to settings
+    settings.bind('disks-list', buildable.get_object('field_disklist'), 'value', Gio.SettingsBindFlags.DEFAULT);
+
+    // Display box
+    box.show_all();
+    return box;
+}
